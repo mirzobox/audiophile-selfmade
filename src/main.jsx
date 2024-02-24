e6bb10be26ad50f0a63172ba48317553b5698db7
mirzobox <mirzoulugbekkhudoyberdiyev@gmail.com>
2024-02-24 12:57:12 +0500

    Add toastify

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,26 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import { Bounce, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <>
+    <Provider store={store}>
+      <App />
+    </Provider>
+    <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="colored"
+      transition={Bounce}
+      limit={3}
+    />
+  </>,
 );
