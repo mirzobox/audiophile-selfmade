4ad59bfee9fbdedc22553277f51dbb6d7accc872
mirzobox <mirzoulugbekkhudoyberdiyev@gmail.com>
2024-02-25 10:30:31 +0500

    Redesign index hero title

--- src/components/IndexHero.jsx
+++ src/components/IndexHero.jsx
@@ -5,7 +5,9 @@ function IndexHero() {
       <div className="base-container py-28 sm:pb-40 sm:pt-32 lg:pb-60 lg:pt-40">
         <div className="mx-auto flex w-full max-w-96 flex-col items-center text-center lg:mx-0 lg:items-start lg:text-start">
           <span className="new-badge mb-4 text-white-50">New product</span>
-          <h1 className="title mb-6 text-white-100">XX99 Mark II Headphones</h1>
+          <h1 className="title mb-6 text-4xl tracking-[1.29px] text-white-100">
+            XX99 Mark II Headphones
+          </h1>
           <p className="mb-7 text-white-75">
             Experience natural, lifelike audio and exceptional build quality
             made for the passionate music enthusiast.
